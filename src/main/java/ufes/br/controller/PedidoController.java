cc5762c7315d1bb07be2750d62fbcc6b5e2b6e8d
Joao Victor <102428176+JvMSan@users.noreply.github.com>
2024-09-28 11:51:53 -0300

    add - GET pedido

--- src/main/java/ufes/br/controller/PedidoController.java
+++ src/main/java/ufes/br/controller/PedidoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ufes.br.pedido.CalculadoraDescontoService;
 import ufes.br.pedido.Pedido;
 import ufes.br.service.PedidoService;
 
@@ -18,5 +19,18 @@ public class PedidoController {
     public ResponseEntity<Pedido> cadastrarPedido(@RequestBody Pedido pedido) {
         Pedido novoPedido = pedidoService.cadastrarPedido(pedido);
         return new ResponseEntity<>(novoPedido, HttpStatus.CREATED);
+    }
+    @GetMapping("/{pedidoID}")
+    public ResponseEntity<Pedido> getDescontos(@PathVariable Integer pedidoID) {
+        Pedido pedidoEncontrado /* = busca no repository aq Repository.getInstance().buscar(pedidoID);*/;
+        return new ResponseEntity<>(pedidoEncontrado, HttpStatus.OK);
+    }
+
+    @PostMapping("/{pedidoID}/processar-descontos")
+    public ResponseEntity<Pedido> processarDescontos(@PathVariable Integer pedidoID) {
+        Pedido pedidoEncontrado /* = busca no repository aq Repository.getInstance().buscar(pedidoID);*/;
+        CalculadoraDescontoService calculadoraDescontoService = new CalculadoraDescontoService();
+        calculadoraDescontoService.processar(pedidoEncontrado);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
